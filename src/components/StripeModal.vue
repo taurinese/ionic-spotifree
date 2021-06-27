4ba2d022cc7e1fdcd41e36f1610f0a8de1042e66
Enzo Taurines <enzodylan.taurines@gmail.com>
2021-06-27 14:13:46 +0200

    Subscriptions works with email and details

--- src/components/StripeModal.vue
+++ src/components/StripeModal.vue
@@ -5,13 +5,84 @@
     </ion-toolbar>
   </ion-header>
   <ion-content class="ion-padding" color="light">
-    <p v-html="getPlan[0].content"></p>
-    <div id="card-element"></div>
+    <!-- <p v-html="getPlan[0].content"></p> -->
+    <IonGrid v-if="!paymentProcessed">
+      <IonRow>
+        <IonCol>
+          <IonItem color="light">
+            <IonLabel position="stacked">Name</IonLabel>
+            <IonInput type="text" v-model="name"></IonInput>
+          </IonItem>
+        </IonCol>
+      </IonRow>
+      <IonRow>
+        <IonCol>
+          <IonItem color="light">
+            <IonLabel position="stacked">Code promo</IonLabel>
+            <IonInput type="text" v-model="coupon"></IonInput>
+          </IonItem>
+        </IonCol>
+      </IonRow>
+      <IonRow>
+        <IonCol>
+          <div id="card-element"></div>
+        </IonCol>
+      </IonRow>
+      <IonRow
+        ><IonButton
+          color="primary"
+          @click="submit"
+          :disabled="payment_processing"
+          >Payer</IonButton
+        ></IonRow
+      >
+    </IonGrid>
+    <IonGrid v-else>
+      <IonRow>
+        <IonCol>{{ messageProcessed }}</IonCol>
+      </IonRow>
+      <IonRow>
+        <IonCol
+          ><p>Prix de l'abonnement</p>
+          <p>{{ getPlan[0].price / 100 }}€</p></IonCol
+        >
+      </IonRow>
+      <IonRow>
+        <IonCol
+          ><p>Date de l'abonnement</p>
+          <p>{{ result[0].created_at }}</p></IonCol
+        >
+      </IonRow>
+      <IonRow>
+        <IonCol
+          ><p>4 derniers chiffres de la CB</p>
+          <p>**** **** **** {{ result.user.card_last_four }}</p></IonCol
+        >
+      </IonRow>
+      <IonRow>
+        <IonCol
+          ><p>Date du renouvellement</p>
+          <p>01/01/2000</p></IonCol
+        >
+      </IonRow>
+    </IonGrid>
   </ion-content>
 </template>
 
 <script>
-import { IonContent, IonHeader, IonTitle, IonToolbar } from "@ionic/vue";
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonItem,
+  IonLabel,
+  IonInput,
+  IonToolbar,
+  IonButton,
+  IonGrid,
+  IonRow,
+  IonCol,
+} from "@ionic/vue";
 import { defineComponent } from "vue";
 import { mapGetters } from "vuex";
 import axios from "axios";
@@ -25,9 +96,29 @@ export default defineComponent({
       secret: null,
       stripe: null,
       card: null,
+      name: "",
+      coupon: "",
+      payment_method: "",
+      payment_processing: false,
+      requiresAction: false,
+      paymentProcessed: false,
+      messageProcessed: "",
+      result: null,
     };
   },
-  components: { IonContent, IonHeader, IonTitle, IonToolbar },
+  components: {
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton,
+    IonGrid,
+    IonRow,
+    IonCol,
+  },
   computed: {
     ...mapGetters(["plans", "token"]),
     getPlan() {
@@ -59,6 +150,66 @@ export default defineComponent({
       .catch((error) => {
         console.log(error);
       });
+  },
+  methods: {
+    resetForm() {
+      this.name = "";
+      this.payment_method = "";
+      this.payment_processing = false;
+      this.coupon = "";
+    },
+    async submit() {
+      this.payment_processing = true;
+      const { setupIntent, error } = await this.stripe.confirmCardSetup(
+        this.secret,
+        {
+          payment_method: {
+            card: this.card,
+            billing_details: { name: this.name },
+          },
+        }
+      );
+      if (error) {
+        console.log(error);
+      } else {
+        console.log(setupIntent);
+        this.payment_method = setupIntent.payment_method;
+        await axios
+          .post(
+            `${process.env.VUE_APP_API_URL}/api/stripe/subscribe`,
+            {
+              plan: this.getPlan[0].id,
+              name: this.name,
+              payment_method: this.payment_method,
+              coupon: this.coupon,
+            },
+            {
+              headers: {
+                Authorization: `Bearer ${this.token}`,
+              },
+            }
+          )
+          .then((response) => {
+            this.result = response.data;
+            console.log(response.data);
+            if (
+              response.data.status &&
+              response.data.status == "requires_action"
+            ) {
+              // Confirmation
+              this.requiresAction = true;
+            } else {
+              this.paymentProcessed = true;
+              this.messageProcessed = "Merci de votre abonnement ! 🤑";
+              console.log("emit 2");
+              this.resetForm();
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
   },
 });
 </script>
